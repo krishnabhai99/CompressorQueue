a1958617ecdee8cd9e47b98a23cd5fe781a5823f
KRISHNA <154213033+krishnabhai99@users.noreply.github.com>
2024-12-25 14:22:34 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -18,9 +18,9 @@ from decouple import config
 try:
     APP_ID = config("APP_ID", "29961447")
     API_HASH = config("API_HASH", "be2e07ad90b5c8b5d368cb4efd302568")
-    BOT_TOKEN = config("BOT_TOKEN", "7392080057:AAEdUMphNSawawaWPNq33-nHLMD8qww8_vA")
-    DEV = 1322549723
-    OWNER = config("OWNER", "5446367898")
+    BOT_TOKEN = config("BOT_TOKEN", "7392080057:AAHB-KJ_ybiteYsBKnPoq2ABh466CmoU818")
+    DEV = 5446367898
+    OWNER = config("5446367898")
     FFMPEG = config(
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
